6e2361fd847510a384f610dd7ef8b14f7136c98d
Xor290 <free6nk33@gmail.com>
2026-04-01 16:26:59 +0200

    chore: add prefixSinceAS gorm model && add createPrefixSinceAS && add PrefixVars

--- api/models/bgp.go
+++ api/models/bgp.go
@@ -11,3 +11,24 @@ type AutonomousSystem struct {
 	Description string `gorm:"size:255"            json:"description"`
 	Peers       []Peer `gorm:"foreignKey:LocalASID" json:"peers,omitempty"`
 }
+
+func (PrefixSinceAS) TableName() string { return "prefix_since_as" }
+
+// PrefixSinceAS représente un préfixe annoncé depuis un AS local vers ses voisins BGP.
+type PrefixSinceAS struct {
+	gorm.Model
+	Prefix    string           `gorm:"size:49;not null" json:"prefix"`    // ex: "192.168.1.0/24"
+	ASID      uint             `gorm:"not null;index"   json:"as_id"`
+	AS        AutonomousSystem `gorm:"foreignKey:ASID"  json:"as,omitempty"`
+	NextHop   string           `gorm:"size:45;not null" json:"next_hop"` // ex: "10.0.0.11"
+	LocalPref uint             `gorm:"default:100"      json:"local_pref"`
+	MED       uint             `gorm:"default:0"        json:"med"`
+	Active    bool             `gorm:"default:true"     json:"active"`   // false = préfixe retiré (withdraw)
+}
+
+type CreatePrefixSinceAS struct {
+	Prefix    string `json:"prefix"` // ex: "192.168.1.0/24"
+	ASN       uint32 `json:"asn"`
+	NextHop   string `json:"next_hop"` // ex: "10.0.0.11"
+	LocalPref uint   `json:"local_pref"`
+}
--- api/models/vtysh.go
+++ api/models/vtysh.go
@@ -8,9 +8,16 @@ type PeerVars struct {
 	Enabled     bool   `yaml:"enabled"`
 }
 
+type PrefixVars struct {
+	Prefix    string `yaml:"prefix"`
+	NextHop   string `yaml:"next_hop"`
+	LocalPref uint   `yaml:"local_pref"`
+}
+
 type PlaybookVars struct {
-	LocalASN uint32     `yaml:"local_asn"`
-	RouterID string     `yaml:"router_id"`
-	Hostname string     `yaml:"hostname"`
-	Peers    []PeerVars `yaml:"peers"`
+	LocalASN uint32       `yaml:"local_asn"`
+	RouterID string       `yaml:"router_id"`
+	Hostname string       `yaml:"hostname"`
+	Peers    []PeerVars   `yaml:"peers"`
+	Prefixes []PrefixVars `yaml:"prefixes"`
 }
